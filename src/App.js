f87daccc0910e93ed80e9a01c17c4db93a424b94
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2022-09-14 17:12:54 -0700

    lab1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <div class="title">
+      <h1>Cloud Lab #1 Assignment</h1>
+      </div>
+      <br></br>
+      <div class="body">
+        <p>This is a cloud lab to review the basics and to gain some hands-on experience on Amazons EC2.</p>
+        <img src="https://codebriefly.com/wp-content/uploads/2022/03/how-to-setup-an-ec2-instance-on-AWS-750x430.jpg" width="700" height="400"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
